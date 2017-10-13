473324d567fd32e4856a12a3a0c70f951c12cd55
Ravi teja Darapu <ravitejadarapu@Ravis-MacBook-Pro.local>
2017-10-14 00:51:04 +0530

    api test code done: added console log

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ router.get('/:appid/webhook/',function(req, res){
 
 router.post('/:appid/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
-    console.log("::: ::: ",req);
+    console.log("::: ::: ",req.body.entry[0]);
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
 	    let sender = event.sender.id
