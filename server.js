30717a9bc70eef0025872eb0bb8ba7dd0249dcb4
Ravi teja Darapu <ravitejadarapu@Ravis-MacBook-Pro.local>
2017-10-13 13:13:08 +0530

    checking router

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ var botAppModel = mongoose.model('botAppModel', botAppSchema);
 router.post('/sendApp',function(req, res){
     console.log('post data; ',req.params);
     console.log('post body; ',req.body);
-    res.send('posted success');
+    res.send(200).json(req);
 });
 
 router.param('appid',function(req, res, next){
